9d6bf69174316d236fb0ee2e301d7afbbba30ad9
twhite17 <twhi0003@student.monash.edu>
2018-09-26 22:20:07 +1000

    Added two main python files. started on assembler.py no substantial progress yet.

---
+++ assembler.py
@@ -0,0 +1,24 @@
+"""
+A Simple MARIE assembler in python 3
+
+Why does this project exist?
+	I developed this assembler (alongside an emulator) because I found it
+	frustrating having to do my FIT1047 assignment in the tiny web editor
+	that was provided to me. With these two tools you will be able to use
+	your own editor of choice and just compile and run your code when you 
+	want to test it.
+
+"""
+
+
+instruction_set = {
+	"ADD":"3",
+	"SUBT":"4",
+	"ADDI":"B",
+	"CLEAR":"A",
+	"LOAD":"1",
+	"STORE":"2",
+	
+
+}
+
