b50e5a9320266df9bb21db4330ea2150760cf7fc
twhite <twhite17t@gmail.com>
2019-07-21 17:31:09 +1000

    added a little more documtentation.

--- assembler.py
+++ assembler.py
@@ -5,7 +5,7 @@ Why does this project exist?
 	I developed this assembler (alongside an emulator) because I found it
 	frustrating having to do my FIT1047 assignment in the tiny web editor
 	that was provided to me. With these two tools you will be able to use
-	your own editor of choice and just compile and run your code when you 
+	your own editor of choice and just compile and run your code when you
 	want to test it.
 
 """
@@ -26,11 +26,11 @@ opcodes = {
 	"STOREI":	"D",
 	"LOADI"	:	"E",
 	"HALT"	:	"7"
-	
+
 	}
 
 includeDirective = {
-	
+
 	"ADD"	:	True,
 	"SUBT"	:	True,
 	"ADDI"	:	True,
@@ -46,50 +46,50 @@ includeDirective = {
 	"STOREI":	True,
 	"LOADI"	:	True,
 	"HALT"	:	False
-	
+
 	}
 
 
-import convert
+import convert # a small library for converting data types
 
 
 class Address:
-	
+
 	def __init__(self, form="dec", addr=0):
 		assert form == "dec" or form == "hex", ValueError("")
 		if form == "dec":
 			self.address = int(addr)
-		else:	
+		else:
 			self.address = convert.hexToDec(start)
-		
+
 	def inc(self):
 		self.address += 1
-	
+
 	def getHex(self):
 		return convert.decToHex(self.address)
 
 class Assembler:
-	
+
 	def __init__(self, filePath="untitled.mas"):
 		self.directives = {}
 		self.build = [] 	# this is where everything is compiled to
 							# every item is an instruction with an opcode and address
 		self.filePath = filePath
 		self.load()
-		
+
 	def load(self, filePath=None):
-		
+
 		# loads file, capitalises all letters and splits it into a list at each newline #
 		if filePath != None:
 			self.filePath = filePath
-		
+
 		with open(self.filePath, "r") as file:
-			
+
 			text = file.read().upper().split("\n")
-		
-		
-		
-		# Removes Comments #######################################
+
+
+
+		# Removes Comments from the marie code.
 		ntext = []
 		for line in text:
 			nline = ""
@@ -98,36 +98,41 @@ class Assembler:
 					nline += ch
 				else:
 					break
-			ntext.append(nline)		
+			ntext.append(nline)
 		text = ntext
-		
-		
-		# Removes empty lines so program takes up less space #
+
+
+		# Removes empty lines so program takes up less space in memory
+        # otherwise the compiled result would have a bunch of memory locations with nothing in them
 		self.code = []
 		for line in text:
 			if line != "":
-				
+
 				x = line.split()
-				
+
 				if x != []:
-				
+
 					self.code.append(x)
-		
-		
-		
+
+
+
 		# where all the assembled code is stored ##############
-		self.build = ["" for i in range(len(self.code))] # there isn't actually any code yet 
-		
-		
+        # there isn't any code yet, this is just data structure setup
+		self.build = ["" for i in range(len(self.code))]
+
+
 	def getDirectives(self):
-		ADRLookup = {}
-		
+
+		ADRLookup = {}    # address that need to be know for variable lookup
+
 		for lineNum in range(len(self.code)):
-			
+
 			try:
 				line = self.code[lineNum]
 				if "," in line[0]:
 					self.directives[line[0].replace(",", "")] = Address(form="dec", addr=lineNum)
+
+                    # Converts all constants to integer representations
 
 					if line[1] == "DEC":
 
@@ -162,16 +167,16 @@ class Assembler:
 				for i in self.code[lineNum]:
 					r += i + " "
 				print("Exception detected in line "+str(lineNum)+ " "+r)
-				
-		
+
+
 		for direct in ADRLookup:
 			self.build[ADRLookup[direct]] = convert.minHexLength(self.directives[direct].getHex(), 4)
-	
+
 	def assemble(self):
 		for lineNum in range(len(self.code)):
 			line = self.code[lineNum]
 			if line[0] in opcodes:
-				
+
 				if includeDirective[line[0]]:
 					try:
 						addr = self.directives[line[1]].getHex()
@@ -180,26 +185,26 @@ class Assembler:
 					self.build[lineNum] = opcodes[line[0]]+addr
 				else:
 					self.build[lineNum] = opcodes[line[0]]+"000"
-	
-				
-			
-	
+
+
+
+
 	def parse(self):
 		self.getDirectives() # loads all directives into a dict for usage in second parse
 		self.assemble() # actually assembles code
-				
+
 
 	def write(self, fileName=None):
 		if fileName == None:
 			fileName = self.filePath.split(".")[0]+".txt"
-		
+
 		result = ""
 		for line in self.build:
 			result += line + '\n'
-		
+
 		with open(fileName, "w") as file:
 			file.write(result[:-1])
-		
+
 
 
 
@@ -209,17 +214,3 @@ if __name__ == "__main__":
 	a.parse()
 	a.write()
 	print("Successfully assembled file \"%s\"" % (f))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
